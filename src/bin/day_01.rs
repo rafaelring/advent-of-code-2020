21f123ac803bae022a97025cde7643d5fb85a8ce
Rafael Ring <rafaelring@gmail.com>
2020-12-22 14:25:42 -0300

    Solve Day 1 B problem

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -5,12 +5,22 @@ fn main() {
 
     let entries = input.lines().map(|line| line.parse().unwrap()).collect();
 
-    println!("Solution 1: {:?}", solve(&entries));
+    println!("Solution A: {:?}", solve_a(&entries));
+    println!("Solution B: {:?}", solve_b(&entries));
 }
 
-fn solve(input: &Vec<i32>) -> i32 {
-    let (x, y) = find_two_numbers_sum(2020, &input).unwrap();
-    x * y
+fn solve_a(input: &Vec<i32>) -> Option<i32> {
+    match find_two_numbers_sum(2020, &input) {
+        Some((x, y)) => return Some(x * y),
+        None => return None,
+    }
+}
+
+fn solve_b(input: &Vec<i32>) -> Option<i32> {
+    match find_three_numbers_sum(2020, &input) {
+        Some((x, y, z)) => return Some(x * y * z),
+        None => return None,
+    }
 }
 
 fn find_two_numbers_sum(desired: i32, data: &Vec<i32>) -> Option<(i32, i32)> {
@@ -23,6 +33,18 @@ fn find_two_numbers_sum(desired: i32, data: &Vec<i32>) -> Option<(i32, i32)> {
                 Some((*first, second))
             } else {
                 None
+            }
+        })
+        .next()
+}
+
+fn find_three_numbers_sum(desired: i32, data: &Vec<i32>) -> Option<(i32, i32, i32)> {
+    data.iter()
+        .filter_map(|first| {
+            let rest = desired - *first;
+            match find_two_numbers_sum(rest, &data) {
+                Some((second, third)) => return Some((*first, second, third)),
+                None => return None,
             }
         })
         .next()
@@ -43,5 +65,21 @@ mod tests {
     #[test]
     fn test_two_sum_not_found() {
         assert_eq!(find_two_numbers_sum(100, &vec![30, 42, 50, 57, 82]), None)
+    }
+
+    #[test]
+    fn test_three_sum_exists() {
+        assert_eq!(
+            find_three_numbers_sum(100, &vec![20, 22, 30, 50, 58, 82]),
+            Some((20, 22, 58))
+        )
+    }
+
+    #[test]
+    fn test_three_sum_not_found() {
+        assert_eq!(
+            find_three_numbers_sum(100, &vec![18, 22, 30, 50, 58, 82]),
+            None
+        )
     }
 }
